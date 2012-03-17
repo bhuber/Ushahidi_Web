91ba73373872818ee12187403421e84a62e8b036
Joe Tricarico <jtricarico@azavea.com>
2012-03-17 17:55:23 -0400

    update twilio library

--- plugins/twilio/controllers/twilio.php
+++ plugins/twilio/controllers/twilio.php
@@ -3,18 +3,34 @@ require('Services/Twilio.php');
 
 class Twilio_Controller extends Controller {
     /* Send SMS via REST API */
-    public function _sms_send($from, $to, $msg)
-    {
-        $client = new Services_Twilio($config['twilio_sid'], $config['twilio_token']);
-        $message = $client->account->sms_messages->create(
-            $from,
-            $to,
-            $msg
-        );
-        return $message->sid;
-    }
+    /* sort of public...can't access over www */
+	private function _get_client()
+	{
+		return new Services_Twilio($config['twilio_sid'], $config['twilio_token']);
+	}
 
-    /* Receive SMS GET request and return TwiML */
+        public function _send_sms($from, $to, $msg)
+        {
+		$client = $this->_get_client();
+		$message = $client->account->sms_messages->create(
+		    $from,
+		    $to,
+		    $msg
+		);
+		return $message->sid;
+        }
+
+	public function _make_call($from, $to, $speech)
+	{
+		$client = $this->_get_client();
+		$call = $client->account->calls->create(
+			$from,
+			$to,
+			'http://'.$_SERVER["SERVER_ADDR"].':'.$_SERVER["SERVER_PORT"].'/twilio/voice?speech='.urlencode($speech)
+		);
+	}
+
+        /* Receive SMS GET request and return TwiML */
 	public function sms()
 	{
 		$view = View::factory('twilio/sms_response');
@@ -22,8 +38,11 @@ class Twilio_Controller extends Controller {
 		$view->render(TRUE);
 	}
 
+	/* Receive voice GET request and return TwiML */
 	public function voice()
 	{
-		//not quite there yet
+		$response = new Services_Twilio_Twiml();
+		$response->say($_GET['speech']);
+		print $response;
 	}
 }
