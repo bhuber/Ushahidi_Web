5dfb03b6b062f90961f92de918adc30ae9293ac5
Bennet Huber <bhuber@azavea.com>
2012-03-18 00:48:00 -0400

    Pad ALL the operators!!1!

--- plugins/smskeepalive/hooks/MessageParser.class.php
+++ plugins/smskeepalive/hooks/MessageParser.class.php
@@ -71,7 +71,7 @@
 
         public function __construct($message, $camefrom, $wentto){
             $elements = explode(" ", $message);
-            $this->message_type=self::find_message_type($elements[0]);
+            $this->message_type = self::find_message_type($elements[0]);
             if (!$this->message_type){
                 throw new Exception(self::ERROR_INVALID_MESSAGE);
             }
@@ -81,69 +81,76 @@
                 case (self::TYPE_CHECK_IN):
                     if ($elements[1] && preg_match(self::REGEX_IDENTITY, $elements[1]))
                     { 
-                        $this->identifier=$elements[1];
+                        $this->identifier = $elements[1];
                         $locationStartIndex = 2;
                     }
                     else
                     {
                         $locationStartIndex=1;
                     }
-                    for ($i = $locationStartIndex; $i < count($elements); $i++){
+
+                    for ($i = $locationStartIndex; $i < count($elements); $i++)
+					{
                         $this->location .= $elements[$i]." ";
                     }
-                    $this->location=trim($this->location);
+                    $this->location = trim($this->location);
                     break;
+
                 case (self::TYPE_CHECKOUT):
                     //Miniumum number of elements is 2
-                    if (count($elements)<2 ) throw new Exception(self::ERROR_MISSING_ELEMENTS);
+                    if (count($elements) < 2)
+						throw new Exception(self::ERROR_MISSING_ELEMENTS);
                     if ($elements[1] && preg_match(self::REGEX_IDENTITY, $elements[1]))
                     { 
-                        $this->identifier=$elements[1];
+                        $this->identifier = $elements[1];
                         $locationStartIndex = 2;
                     }
                     else
                     {
-                        $locationStartIndex=1;
+                        $locationStartIndex = 1;
                     }
-                    $passwordIndex=count($elements)-1;
-                    $this->password=$elements[$passwordIndex];
+                    $passwordIndex = count($elements)-1;
+                    $this->password = $elements[$passwordIndex];
                     for ($i=$locationStartIndex; $i<$passwordIndex; $i++)
                     {
-                        $this->location.=$elements[$i]." ";
+                        $this->location .= $elements[$i]." ";
                     }
-                    $this->location=trim($this->location);
+                    $this->location = trim($this->location);
                     break;
+
                 case (self::TYPE_LOCATION):
                     if ($elements[1] && preg_match(self::REGEX_IDENTITY, $elements[1]))
                     { 
-                        $this->identifier=$elements[1];
+                        $this->identifier = $elements[1];
                         $locationStartIndex = 2;
                     }
                     else
                     {
-                        $locationStartIndex=1;
+                        $locationStartIndex = 1;
                     }
                     for ($i = $locationStartIndex; $i<count($elements); $i++){
-                        $this->location.=$elements[$i]." ";
+                        $this->location .= $elements[$i]." ";
                     }
-                    $this->location=trim($this->location);
+                    $this->location = trim($this->location);
                     break;
+
                 case (self::TYPE_HELP):
                 case (self::TYPE_STATUS):
                     if ($elements[1] && preg_match(self::REGEX_IDENTITY, $elements[1]))
                     { 
-                        $this->identifier=$elements[1];
+                        $this->identifier = $elements[1];
                         $messageStartIndex = 2;
                     }
                     else
                     {
-                        $messageStartIndex=1;
+                        $messageStartIndex = 1;
                     }
-                    for ($i = $messageStartIndex; $i<=count($elements); $i++){
-                        $this->message.=$elements[$i]." ";
+                    for ($i = $messageStartIndex; $i < count($elements); $i++){
+                        $this->message .= $elements[$i]." ";
                     }
-                    $this->message=trim($this->message);
+                    $this->message = trim($this->message);
                     break;
+
                 case (self::TYPE_ALL_CLEAR):
                     break;
             }
@@ -154,32 +161,32 @@
             $token = strtolower($token);
             foreach(explode(",",self::TOKENS_TYPE_LOCATION) as $t)
             {
-                if($token==$t) return self::TYPE_LOCATION;
+                if($token == $t) return self::TYPE_LOCATION;
             }
 
             foreach(explode(",",self::TOKENS_TYPE_CHECKIN) as $t)
             {
-                if($token==$t) return self::TYPE_CHECK_IN;
+                if($token == $t) return self::TYPE_CHECK_IN;
             }
 
             foreach(explode(",",self::TOKENS_TYPE_CHECKOUT) as $t)
             {
-                if($token==$t) return self::TYPE_CHECKOUT;
+                if($token == $t) return self::TYPE_CHECKOUT;
             }
 
             foreach(explode(",",self::TOKENS_TYPE_HELP) as $t)
             {
-                if($token==$t) return self::TYPE_HELP;
+                if($token == $t) return self::TYPE_HELP;
             }
 
             foreach(explode(",",self::TOKENS_TYPE_ALL_CLEAR) as $t)
             {
-                if($token==$t) return self::TYPE_ALL_CLEAR;
+                if($token == $t) return self::TYPE_ALL_CLEAR;
             }
 
             foreach(explode(",",self::TOKENS_TYPE_STATUS) as $t)
             {
-                if($token==$t) return self::TYPE_STATUS;
+                if($token == $t) return self::TYPE_STATUS;
             }
             return null;
 
