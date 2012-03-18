70ad14c7cd3e3d0f164b82c35dec224b21b46376
Joe Tricarico <jtricarico@azavea.com>
2012-03-18 02:44:31 -0400

    err handling improvements

--- plugins/twilio/controllers/twilio.php
+++ plugins/twilio/controllers/twilio.php
@@ -31,10 +31,14 @@ class Twilio_Controller extends Controller {
         /* Receive SMS GET request and return TwiML */
 	public function sms()
 	{
-		$response = sms::add($_GET['From'], $_GET['Body']);
-		if($response !== FALSE)
-		{
-			 $response = 'Message received.';
+		try {
+			$response = sms::add($_GET['From'], $_GET['Body']);
+			if($response === TRUE)
+			{
+				 $response = 'Message received.';
+			}
+		} catch(Exception $e) {
+			$response = substr($e->getMessage(), 0, 160);
 		}
 		$view = View::factory('twilio/sms_response');
 		$view->response = $response;
